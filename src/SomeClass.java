6e14984c0ec9f8ace602f9fb5f4b09d675d8eb88
Matt Clayton <Matt Clayton>
2016-10-06 19:48:59 -0600

    Created SomeClass class and added fnOne to it

---
+++ src/SomeClass.java
@@ -0,0 +1,13 @@
+
+public class SomeClass {
+	public void fnOne() {
+		System.out.println("I couldn't repair your brakes, so I made your horn louder.");
+	}
+
+	public static void main(String[] args) {
+		SomeClass sc = new SomeClass();
+		sc.fnOne();
+
+	}
+
+}
