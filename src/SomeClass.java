bd32ee24ae96e5f577bbc08e1227adeaef4f5712
Matt Clayton <Matt Clayton>
2016-10-06 19:53:42 -0600

    Added upcomingEvents class

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -3,10 +3,15 @@ public class SomeClass {
 	public void thoughtForTheDay() {
 		System.out.println("I couldn't repair your brakes, so I made your horn louder.");
 	}
+	
+	public void upcomingEvents() {
+		System.out.println("Party at Jane's house tomorrow.");
+	}
 
 	public static void main(String[] args) {
 		SomeClass sc = new SomeClass();
 		sc.thoughtForTheDay();
+		sc.upcomingEvents();
 
 	}
 
