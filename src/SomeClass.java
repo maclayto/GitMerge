d8de446ee10919aaf28f004db751cf9dc8e19b1a
Matt Clayton <Matt Clayton>
2016-10-06 20:23:40 -0600

    Added the Motivator

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -18,12 +18,24 @@ public class SomeClass {
 			System.out.println(event);
 		}
 	}
+	
+	public void feedback(int score) {
+		if (score == 100)
+			System.out.println("You're awesome");
+		else if (score > 90)
+			System.out.println("That's great");
+		else if (score > 60)
+			System.out.println("That's good ");
+		else
+			System.out.println("Well, what can I say?");
+	}
 
 	public static void main(String[] args) {
 		SomeClass sc = new SomeClass();
 		sc.thoughtForTheDay();
 		sc.createEvents();
 		sc.upcomingEvents();
+		sc.feedback(60);
 
 	}
 
